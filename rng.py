a4e228b80bec0ccf1181a6b789c0857875448b30
Danny Dawson <121729923+danny-dawson@users.noreply.github.com>
2024-04-30 15:01:49 -0400

    Relational Reasoning Prompt Generation

---
+++ rng.py
@@ -0,0 +1,17 @@
+import random 
+import numpy as np
+
+def relational_test(n):
+    
+    x = random.randrange(1, n)
+    y = random.randint(1, n)
+
+    print(x, y)
+    print("What is the relationship between P",x, "and P",y,"?")
+
+#print(relational_test(10))
+#print(relational_test(10))
+#print(relational_test(10))
+#print(relational_test(10))
+#print(relational_test(10))
+
